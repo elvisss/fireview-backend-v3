e420263091141eb3eea64588213f721326ab8405
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2021-11-14 00:45:02 -0500

    adding config file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import { config } from './config';
 
 const app = express();
-const port = 3000;
+const port = config.port;
 
 app.get('/', (_req, res) => {
 	res.send('The sedulous hyena ate the antelope!');
